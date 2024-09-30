176689e67adb9dc36ef009e59e25ad428125c9f0
b.smirnov <besm1@yandex.ru>
2024-09-30 20:22:02 +0300

    Commit 9_3

---
+++ module_9_6.py
@@ -0,0 +1,14 @@
+def all_variants(text: str):
+    ln = 1
+    while ln <= len(text):
+        i = 0
+        while i <= len(text)-ln:
+            yield text[i: i + ln ]
+            i += 1
+        ln += 1
+
+v = all_variants('abc')
+
+for v_ in v:
+    print(v_)
+
